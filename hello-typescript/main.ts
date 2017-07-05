e8a6874afdcd3502ff140ae71a0a03d31ee6cfbd
Everton Nogueira <everton.nogueira@gohiper.com.br>
2017-07-05 00:11:45 -0300

    Hello typescript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,26 @@
+var minhaVar = "minha variavel";
+
+function minhaFuncao(x,y){
+    return x+y;
+}
+
+let num = 2;
+
+const PI = 3.14;
+
+
+var numeros=[1,2,3];
+
+
+numeros.map(function(v){return v*2});
+numeros.map(v=>v*2);
+
+class Matematica{
+    soma(x,y){
+        return x+y;
+    }
+}
+
+
+var n1: string ='dafas';
+n1=4;
